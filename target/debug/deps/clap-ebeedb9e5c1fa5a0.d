34de5ec8c8a14bb10770c4919b7aca219463306f
keita.g <keita.g@collabo-style.co.jp>
2025-08-27 08:25:11 +0900

    Remove target directory from Git tracking

--- target/debug/deps/clap-ebeedb9e5c1fa5a0.d
+++
@@ -1,9 +0,0 @@
-/Users/gotoukei/Desktop/pyro_lang_test/target/debug/deps/clap-ebeedb9e5c1fa5a0.d: /Users/gotoukei/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.5.45/src/lib.rs /Users/gotoukei/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.5.45/src/../examples/demo.rs /Users/gotoukei/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.5.45/src/../examples/demo.md
-
-/Users/gotoukei/Desktop/pyro_lang_test/target/debug/deps/libclap-ebeedb9e5c1fa5a0.rlib: /Users/gotoukei/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.5.45/src/lib.rs /Users/gotoukei/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.5.45/src/../examples/demo.rs /Users/gotoukei/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.5.45/src/../examples/demo.md
-
-/Users/gotoukei/Desktop/pyro_lang_test/target/debug/deps/libclap-ebeedb9e5c1fa5a0.rmeta: /Users/gotoukei/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.5.45/src/lib.rs /Users/gotoukei/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.5.45/src/../examples/demo.rs /Users/gotoukei/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.5.45/src/../examples/demo.md
-
-/Users/gotoukei/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.5.45/src/lib.rs:
-/Users/gotoukei/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.5.45/src/../examples/demo.rs:
-/Users/gotoukei/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.5.45/src/../examples/demo.md:
